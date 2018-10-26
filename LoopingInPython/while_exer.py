9643ecf79275fcc68d8caab27e7e14ed33384bd0
Darryl Lardizabal <dsteven12@gmail.com>
2018-10-26 23:19:00 +0000

    Created a for loop that displays number of smileys in a half triangle formation.

---
+++ LoopingInPython/while_exer.py
@@ -0,0 +1,20 @@
+def userNum():
+    """Takes USER input to be used to display ith number of smileys"""
+    nums = int(input("Please give an integer: "))
+    if((nums == 1) or (nums ==0)):
+        nums = int(input("Please give an integer that isn't 0 or 1: "))
+    nums += 1
+    return nums
+
+def smileyPrint(userInput):
+    """Takes int generated from userNum() and displays 
+    smileys * int selected by user"""
+    for num in range(1, userInput):
+        print("\U0001f600" * num)
+    
+def main():
+    userInput = userNum()
+    smileyPrint(userInput)
+    
+main()
+    
